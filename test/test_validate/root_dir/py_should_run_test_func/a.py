9cee96cd9a29cec99cacfe94f3273ec8bfc7fa9f
laixintao <laixintaoo@gmail.com>
2023-03-29 15:09:31 +0800

    add testcase

---
+++ test/test_validate/root_dir/py_should_run_test_func/a.py
@@ -0,0 +1,11 @@
+def generate_targets(**args):
+    return [
+        {
+            "labels": {"arg_foo": args["foo"]},
+            "targets": ["127.0.0.1:8080"],
+        }
+    ]
+
+
+def test_generate_targets():
+    return generate_targets(foo="bar")
