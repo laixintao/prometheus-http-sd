6f39e907c1fa77a4987b9a0e2d2f7e4d3d8bb0d1
laixintao <laixintaoo@gmail.com>
2024-09-17 16:27:50 +0800

    adding test.

---
+++ test/app_root/echo_target/sleep2_target.py
@@ -0,0 +1,16 @@
+import time
+
+
+def generate_targets():
+    """Return arg list in label target."""
+    time.sleep(2)
+    return [
+        {
+            "labels": {"sleep": "2"},
+            "targets": ["127.0.0.1:8080"],
+        }
+    ]
+
+
+if __name__ == "__main__":
+    generate_targets()
